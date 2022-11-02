ad601a5782aa0c25266b465c9be5a810d090fce5
febryanmz <febryan.zainal@gmail.com>
2022-11-03 00:05:07 +0700

    assignment

--- problem1/primex.go
+++ problem1/primex.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func PrimeX(number int) int {
+	var x = []int{1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29}
+	// fmt.Println(x[number])
+	return int(x[number])
 
 }
 
--- problem2/fibonacci.go
+++ problem2/fibonacci.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func Fibonacci(number int) int {
-
+	// var n int
+	if number <= 1 {
+		return number
+	}
+	return Fibonacci(number-1) + Fibonacci(number-2)
+	// return Fibonacci(number)
 }
 
 func main() {
@@ -13,5 +18,10 @@ func main() {
 	fmt.Println(Fibonacci(9))  // 34
 	fmt.Println(Fibonacci(10)) // 55
 	fmt.Println(Fibonacci(12)) // 144
+	fmt.Println(Fibonacci(20)) //
 
 }
+
+// f(9)=f(8+7) // 8? 7?(6+5)
+// f(8)=f(7+6) // 7? 6?(5+4)
+// f(7)=f(6+5) // 6? 5?(4+3)
